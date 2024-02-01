cf6e6e069161e9629abcd87dc8d3d2644989c4b9
João Pedro Alves da Silva <pingulim.br@gmail.com>
2024-02-01 12:37:24 -0300

    feat: create api call

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
+require("dotenv").config()
+
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) { }
+
+  getOwnedGames() {
+    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.SteamAPIKey}&steamid=${process.env.SteamUserID}&include_appinfo=true&include_played_free_games=true&format=json`
+
+    const result = this.httpService
+      .get(url)
+      .pipe(
+        map(response => (response.data))
+      )
+
+    return result
   }
 }
